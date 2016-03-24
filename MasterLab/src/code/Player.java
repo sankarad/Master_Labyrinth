460e6f61fac075d08487f45a6089e3c2cb5766b3
Sankara Daly <Sankara Daly@Shomari>
2016-03-24 15:37:15 -0400

    sANKARA

--- MasterLab/src/code/Player.java
+++ MasterLab/src/code/Player.java
@@ -11,20 +11,17 @@ public class Player {
 	
 	
 	
-	
-	
 	/*This class determines the ability of a player to legally
 	 * move to a predetermined position indicated. If it is unable
 	 * to move to the declared space it should return false or true.
 	 */
 	public boolean Path(){
 		boolean x= false;
-		Object tile;
-		if(tile.isLegal()== true)
-		{
-			//Player position is equal to new postion 
-		}
-		return false;
+		Object t1,t2,t3,t4;
+	}
+	public Integer isLegal()
+	{
+		
 	}
 	
 	}
