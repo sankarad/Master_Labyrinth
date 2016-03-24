f421b6a140e9552c729dbd004614df1660b3df86
Sankara Daly <Sankara Daly@Shomari>
2016-03-24 14:38:34 -0400

    new stuff

---
+++ MasterLab/src/code/Pawns.java
@@ -0,0 +1,18 @@
+package code;
+
+import java.awt.Color;
+
+public class Pawns  {
+	private Color _pcolor;
+	
+	public Pawns(){
+		Pawns p= new Pawns();
+		_pcolor=Color.BLACK;
+		
+		
+		
+	}
+	
+	
+
+	}
---
+++ MasterLab/src/code/Player.java
@@ -0,0 +1,30 @@
+package code;
+import java.awt.Color;
+public class Player {
+	private Color _pcolor;
+	private Pawns _a;
+
+	public Player(Pawns a, Color p){
+		_a=a;
+		_pcolor=p;	
+	}
+	
+	
+	
+	
+	
+	/*This class determines the ability of a player to legally
+	 * move to a predetermined position indicated. If it is unable
+	 * to move to the declared space it should return false or true.
+	 */
+	public boolean Path(){
+		boolean x= false;
+		Object tile;
+		if(tile.isLegal()== true)
+		{
+			//Player position is equal to new postion 
+		}
+		return false;
+	}
+	
+	}
